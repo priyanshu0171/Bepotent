95b534298536525202507a2947a32f68d237445c
Priyanshu Mishra <priyanshu0171@gmail.com>
2021-10-22 22:15:46 +0530

    Restructured Project Structure

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 #import libraries
-import numpy as np
 from flask import Flask, render_template,request,redirect, url_for,request,make_response
 import requests
-import pickle
 import pymongo
 import bcrypt
 import datetime
 #Initialize the flask App
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
+
 
 app.secret_key = "testing"
 client = pymongo.MongoClient("mongodb+srv://Bepotent:12345@cluster0.f8pdb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
@@ -57,9 +55,9 @@ def form():
             email = request.cookies.get('email')
             records.update( {"email":email},{"$set":input},upsert=True)
            
-        else:
-            return render_template('index.html')        
         return render_template('vitals_form.html')
+    else:
+        return render_template('index.html')        
     
 
 
@@ -137,46 +135,6 @@ def signup():
                 return render_template('login.html', email=new_email)
     return render_template('signup.html')
 
-
-@app.route("/predict-diabetes", methods=["GET","POST"])
-def predictor():
-    return render_template('predictor.html')
-
-@app.route("/predict", methods=["POST"])
-def predict():
-
-    prg = request.form['prg']
-    glc = request.form['glc']
-    bp = request.form['bp']
-    skt = request.form['skt']
-    ins = request.form['ins']
-    bmi = request.form['bmi']
-    dpf = request.form['ped']
-    age = request.form['age']
-
-    prg = int(prg)
-    glc = int(glc)
-    bp = int(bp)
-    skt = int(skt)
-    ins = int(ins)
-    bmi = float(bmi)
-    dpf = float(dpf)
-    age = int(age)
-
-    final_features = np.array([(prg, glc, bp, skt, ins, bmi, dpf, age)])
-    sc=pickle.load(open('scaler.sav','rb'))
-    final_features=sc.transform(final_features)
-
-    prediction = model.predict(final_features)
-
-    if prediction == 1:
-        text = 'have diabetes'
-    else:
-        text = 'does not have diabetes'
-    text = f'The patient {text}'
-    print(prediction)
-    print(text)
-    return render_template("predictor.html", prediction_text = text, prediction=prediction)
 
 @app.route('/excercise')
 def excercise():
